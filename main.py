c467fe3f500ca1ba8d0328f23c4edc4f1900990c
Sunopportunity <88450042+Sunopportunity@users.noreply.github.com>
2024-05-05 09:52:41 +0300

    внесены изменения

---
+++ main.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+def on_button_click():
+    label.config(text=f"Привет, {entry.get()}!")
+
+
+
+
+root = tk.Tk()
+root.title("АНКЕТА")
+# Тестовый коммент
+label = tk.Label(root, text="Введите свои имя и фамилию")
+label.pack()
+
+entry = tk.Entry()
+entry.pack()
+
+button = tk.Button(root, text="OK", command=on_button_click)
+button.pack()
+
+root.mainloop()
+
+
+
+
+
+print("test")
